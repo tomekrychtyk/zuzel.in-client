84420c19979c6e355740ca7e47b13fec1c40f03a
Tomek Rychtyk <tomek@rychtyk.pl>
2023-04-27 17:38:49 +0200

    Table view

--- src/features/dashboard/ExtraLeague/LeagueTable.tsx
+++ src/features/dashboard/ExtraLeague/LeagueTable.tsx
@@ -1,4 +1,46 @@
-import { Typography, Card, Box, useTheme } from '@mui/material';
+import { Fragment } from 'react';
+import {
+  useTheme,
+  styled,
+  Typography,
+  Card,
+  Box,
+  Divider,
+  Avatar,
+  TableContainer,
+  TableHead,
+  Table,
+  TableRow,
+  TableCell,
+  TableBody
+} from '@mui/material';
+import table from './data/table';
+
+const LOGOS_BUCKET_URL = 'https://s3.eu-central-1.amazonaws.com/zuzel.in/logos';
+
+const TableHeadWrapper = styled(TableHead)(
+  ({ theme }) => `
+      .MuiTableCell-root {
+          text-transform: none;
+          font-size: ${theme.typography.pxToRem(14)};
+          padding-top: 0;
+          padding-bottom: ${theme.spacing(1)};
+          color: ${theme.colors.alpha.black[100]};
+      }
+
+      .MuiTableRow-root {
+          background: transparent;
+      }
+  `
+);
+
+const TableWrapper = styled(Table)(
+  () => `
+    .MuiTableCell-root {
+        border-bottom: 0;
+    }
+  `
+);
 
 export default function LeagueTable() {
   const theme = useTheme();
@@ -24,8 +66,56 @@ export default function LeagueTable() {
         >
           PGE Ekstraliga
         </Typography>
-        <Typography variant="h4">Tabela</Typography>
+        <Typography variant="h4">Ostatnie mecze</Typography>
       </Box>
+      <Divider />
+      <TableContainer sx={{ pt: 1 }}>
+        <TableWrapper>
+          <TableHeadWrapper>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>M</TableCell>
+            <TableCell>P</TableCell>
+          </TableHeadWrapper>
+          <TableBody>
+            {table.map(({ place, name, matches, points, logo }) => {
+              return (
+                <TableRow hover>
+                  <TableCell align="right">{place}</TableCell>
+                  <TableCell>
+                    <Box display="flex" alignItems="center">
+                      <Avatar
+                        sx={{
+                          color: `${theme.colors.error.main}`,
+                          width: 40,
+                          height: 40
+                        }}
+                        src={`${LOGOS_BUCKET_URL}/${logo}`}
+                      />
+                      <Box ml={1.5}>
+                        <Typography
+                          color="text.primary"
+                          variant="h5"
+                          noWrap
+                          sx={{ fontSize: '12px' }}
+                        >
+                          {name}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography variant="h5">{matches}</Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography variant="h5">{points}</Typography>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </TableWrapper>
+      </TableContainer>
     </Card>
   );
 }
--- src/features/dashboard/ExtraLeague/NextMatches.tsx
+++ src/features/dashboard/ExtraLeague/NextMatches.tsx
@@ -21,140 +21,147 @@ export default function NextMatches() {
   return (
     <Card
       sx={{
-        height: '100%'
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
       }}
     >
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection="column"
-        p={2}
-      >
-        <Typography
-          variant="caption"
-          fontWeight="bold"
-          sx={{
-            fontSize: `${theme.typography.pxToRem(12)}`
-          }}
+      <Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection="column"
+          p={2}
         >
-          PGE Ekstraliga
-        </Typography>
-        <Typography variant="h4">Nadchodzące mecze</Typography>
+          <Typography
+            variant="caption"
+            fontWeight="bold"
+            sx={{
+              fontSize: `${theme.typography.pxToRem(12)}`
+            }}
+          >
+            PGE Ekstraliga
+          </Typography>
+          <Typography variant="h4">Nadchodzące mecze</Typography>
+        </Box>
+        <Divider />
+        <List disablePadding>
+          {nextMatches.map((matchDay) => {
+            return (
+              <Fragment key={matchDay.date}>
+                <ListSubheader
+                  color="primary"
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  {matchDay.date}
+                </ListSubheader>
+                <Divider />
+                {matchDay.matches.map((match) => {
+                  return (
+                    <Fragment key={match.id}>
+                      <ListItem
+                        sx={{
+                          py: 2,
+                          display: 'flex',
+                          justifyContent: 'space-between'
+                        }}
+                      >
+                        <ListItemAvatar
+                          sx={{
+                            minWidth: 'auto',
+                            mr: 2,
+                            mb: { xs: 2, sm: 0 }
+                          }}
+                        >
+                          <Avatar
+                            sx={{
+                              width: 42,
+                              height: 42
+                            }}
+                            alt="Krosno"
+                            src={`${LOGOS_BUCKET_URL}/${match.homeLogo}`}
+                          />
+                        </ListItemAvatar>
+                        <ListItemText
+                          sx={{
+                            flexGrow: 0,
+                            maxWidth: '50%',
+                            flexBasis: '50%'
+                          }}
+                          disableTypography
+                          primary={
+                            <Typography color="text.primary" variant="h5">
+                              {match.home}
+                            </Typography>
+                          }
+                        />
+                        <ListItemText
+                          sx={{
+                            flexGrow: 0,
+                            maxWidth: '50%',
+                            flexBasis: '50%'
+                          }}
+                          disableTypography
+                          primary={
+                            <Typography
+                              color="text.primary"
+                              variant="h5"
+                              sx={{ textAlign: 'center' }}
+                            >
+                              - : -
+                            </Typography>
+                          }
+                        />
+                        <ListItemText
+                          sx={{
+                            flexGrow: 0,
+                            maxWidth: '50%',
+                            flexBasis: '50%'
+                          }}
+                          disableTypography
+                          primary={
+                            <Typography
+                              color="text.primary"
+                              variant="h5"
+                              sx={{ textAlign: 'right' }}
+                            >
+                              {match.away}
+                            </Typography>
+                          }
+                        />
+                        <ListItemAvatar
+                          sx={{
+                            minWidth: 'auto',
+                            ml: 2,
+                            mb: { xs: 2, sm: 0 }
+                          }}
+                        >
+                          <Avatar
+                            sx={{
+                              width: 42,
+                              height: 42
+                            }}
+                            alt={match.away}
+                            src={`${LOGOS_BUCKET_URL}/${match.awayLogo}`}
+                          />
+                        </ListItemAvatar>
+                      </ListItem>
+                      <Divider />
+                    </Fragment>
+                  );
+                })}
+              </Fragment>
+            );
+          })}
+        </List>
       </Box>
-      <List disablePadding>
-        {nextMatches.map((matchDay) => {
-          return (
-            <Fragment key={matchDay.date}>
-              <ListSubheader
-                color="primary"
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between'
-                }}
-              >
-                {matchDay.date}
-              </ListSubheader>
-              <Divider />
-              {matchDay.matches.map((match) => {
-                return (
-                  <Fragment key={match.id}>
-                    <ListItem
-                      sx={{
-                        py: 2,
-                        display: 'flex',
-                        justifyContent: 'space-between'
-                      }}
-                    >
-                      <ListItemAvatar
-                        sx={{
-                          minWidth: 'auto',
-                          mr: 2,
-                          mb: { xs: 2, sm: 0 }
-                        }}
-                      >
-                        <Avatar
-                          sx={{
-                            width: 42,
-                            height: 42
-                          }}
-                          alt="Krosno"
-                          src={`${LOGOS_BUCKET_URL}/${match.homeLogo}`}
-                        />
-                      </ListItemAvatar>
-                      <ListItemText
-                        sx={{
-                          flexGrow: 0,
-                          maxWidth: '50%',
-                          flexBasis: '50%'
-                        }}
-                        disableTypography
-                        primary={
-                          <Typography color="text.primary" variant="h5">
-                            {match.home}
-                          </Typography>
-                        }
-                      />
-                      <ListItemText
-                        sx={{
-                          flexGrow: 0,
-                          maxWidth: '50%',
-                          flexBasis: '50%'
-                        }}
-                        disableTypography
-                        primary={
-                          <Typography
-                            color="text.primary"
-                            variant="h5"
-                            sx={{ textAlign: 'center' }}
-                          >
-                            - : -
-                          </Typography>
-                        }
-                      />
-                      <ListItemText
-                        sx={{
-                          flexGrow: 0,
-                          maxWidth: '50%',
-                          flexBasis: '50%'
-                        }}
-                        disableTypography
-                        primary={
-                          <Typography
-                            color="text.primary"
-                            variant="h5"
-                            sx={{ textAlign: 'right' }}
-                          >
-                            {match.away}
-                          </Typography>
-                        }
-                      />
-                      <ListItemAvatar
-                        sx={{
-                          minWidth: 'auto',
-                          ml: 2,
-                          mb: { xs: 2, sm: 0 }
-                        }}
-                      >
-                        <Avatar
-                          sx={{
-                            width: 42,
-                            height: 42
-                          }}
-                          alt={match.away}
-                          src={`${LOGOS_BUCKET_URL}/${match.awayLogo}`}
-                        />
-                      </ListItemAvatar>
-                    </ListItem>
-                    <Divider />
-                  </Fragment>
-                );
-              })}
-            </Fragment>
-          );
-        })}
-      </List>
+      <Box>dsfsdf</Box>
     </Card>
   );
 }
