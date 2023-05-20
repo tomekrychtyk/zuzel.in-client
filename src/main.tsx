09f1a27808cb0399cf4dd13cbdf4397b5f4c1aae
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-20 16:22:22 +0200

    Configured Redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
 );
