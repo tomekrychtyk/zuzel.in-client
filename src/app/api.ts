5ea0fe0e9de880293de5a9f457a7e2b86cbb9475
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-23 13:26:23 +0200

    Add default API url

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || 'https://speedwayapi.pl/v1';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
